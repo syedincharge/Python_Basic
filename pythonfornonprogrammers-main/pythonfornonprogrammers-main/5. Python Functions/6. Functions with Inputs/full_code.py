069c7d650c723fe1feac64a1ba70aff65a8e563a
syedincharge <syedrizvidev007@gmail.com>
2025-10-18 10:14:01 -0500

    add functions

--- pythonfornonprogrammers-main/pythonfornonprogrammers-main/5. Python Functions/6. Functions with Inputs/full_code.py
+++ pythonfornonprogrammers-main/pythonfornonprogrammers-main/5. Python Functions/6. Functions with Inputs/full_code.py
@@ -12,9 +12,31 @@ def greet():
 
 # Function with input
 
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How are you {name}")
+    
+#greet_with_name("Syed")
+
+
+def greet_with_full_name(first_name, last_name):
+    print(f"Hello {first_name} {last_name}")
+    print(f"How are you {first_name} {last_name}")
+greet_with_full_name("Syed", "Rizvi")
+
+
 def greet_with_name(name):
     print(f"Hello {name}")
     print(f"How are you {name}")
+
+
+
+def greet_with_Name_location(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
     
-greet_with_name("Syed")
+greet_with_Name_location("Syed", "New York")    
+    
+    
+
 
